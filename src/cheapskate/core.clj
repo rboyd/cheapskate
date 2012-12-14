a08924407d5540bad945abd0d773be3dd826c349
Robert Boyd <rboyd@telematter.com>
2012-12-14 00:39:40 -0600

    Updated xml-from-api to take optional query params. Preemptively added paging-results fn to get ready for auto-paging.

--- src/cheapskate/core.clj
+++ src/cheapskate/core.clj
@@ -4,14 +4,20 @@
             [net.cgrand.enlive-html :as html]
             [net.cgrand.xml :as xml]))
 
-(defn xml-from-api [cmd]
-  (:body (client/get (config :service-url) {:query-params (merge (config :auth) {:Command cmd}) })))
+(defn xml-from-api [cmd & params]
+  (let [query-params (merge {:Command cmd} (config :auth) (apply merge params))]
+    (:body (client/get (config :service-url) {:query-params query-params}))))
 
 (defn parse-response [xml]
  (xml/parse (java.io.ByteArrayInputStream. (.getBytes xml))))
 
 (defn unbox-domains [edn]
- (map :attrs (html/select edn [:Domain])))
+  (map :attrs (html/select edn [:Domain])))
+
+(defn paging-results [edn]
+  (let [paging-elements [:TotalItems :CurrentPage :PageSize]
+        paging-values (map #(-> (html/select edn [:Paging % html/content]) first Integer/parseInt) paging-elements)]
+    (zipmap paging-elements paging-values)))
 
 (defn get-domains []
- (-> (xml-from-api "namecheap.domains.getList") parse-response unbox-domains))
+  (-> (xml-from-api "namecheap.domains.getList" {:PageSize 100}) parse-response unbox-domains))
